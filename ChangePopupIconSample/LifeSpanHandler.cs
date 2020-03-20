c5ec9010d53b0490ffb4e15cdd9fbeaf914131e1
Takumi Oda <xiaotiantakumi@gmail.com>
2020-03-20 09:17:20 +0900

    ポップアップのアイコン変更のサンプル作成用に新規フォルダ追加等

---
+++ ChangePopupIconSample/LifeSpanHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CefSharp;
+
+namespace WinFormsCefSharpSample.ChangePopupIconSample
+{
+    class LifespanHandler : ILifeSpanHandler
+    {
+        /// <summary>
+        /// popupを呼び出すためのアクション
+        /// </summary>
+        public Action<string> raisePopupForm;
+        public bool OnBeforePopup(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, string targetUrl,
+            string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures,
+            IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
+        {
+            // 設定した
+            raisePopupForm?.Invoke(targetUrl);
+            newBrowser = null;
+            return true;
+        }
+
+        public void OnAfterCreated(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+        }
+
+        public bool DoClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            return false;
+        }
+
+        public void OnBeforeClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+        }
+    }
+}
---
+++ ChangePopupIconSample/PopupForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace ChangePopupIconSample.JsMessageSample
+{
+    public partial class PopupForm : Form
+    {
+        private readonly string _url;
+
+        public PopupForm(string url)
+        {
+            _url = url;
+            InitializeComponent();
+            InitializeChromium();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        public ChromiumWebBrowser chromeBrowser;
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // CefのInitializeは一度だけ
+            if (!Cef.IsInitialized)
+            {
+                // Initialize cef with the provided settings
+                Cef.Initialize(settings);
+                // これを入れないと黒い余白が発生していまう。
+                Cef.EnableHighDPISupport();
+            }
+
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser(this._url);
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+    }
+}
